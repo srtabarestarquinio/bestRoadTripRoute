c28357b84c8b6e1fcd4f337d400ff85ad4988695
srtabarestarquinio <49605681+srtabarestarquinio@users.noreply.github.com>
2020-11-23 14:17:59 -0600

    Update leastDistanceRoute.java

--- leastDistanceRoute.java
+++ leastDistanceRoute.java
@@ -10,25 +10,27 @@ import java.io.*;
 
 public class leastDistanceRoute{
   
-	public String [] route(String starting_city, String ending_city, String [] attractions){
+	public List<String> route(String starting_city, String ending_city, List<String> attractions){
  		//Creates empty list of locations of size of List<String> attractions + 2 (for starting and ending city) called List<String> locationsList
-		int numLocations = attractions.length + 2;
-		String [] locationsList = new String [numLocations];
+		int numLocations = attractions.getSize() + 2;
+		List<String> locationsList = new List(numLocations);
 		//Add starting_city in position 0 and ending_city in position -1 of locationsList attractionsLocations( attractions, locationsList )
-		locationsList[0] = starting_city;
-		locationsList[numLocations-1] = ending_city;
+		locationsList.add(0, starting_city);
+		locationsList.add(numLocations-1, ending_city);
+		return locationsList;
 		//add locations of attractions to locationList with function attractionslocations()
-		locationsList = attractionsLocations(attractions, locationsList);
-		
+		// locationsList = attractionsLocations(attractions, locationsList);
 		//Use Dijkstra’s algorithm to calculate shortest distances and add them to List<String> route = Dijkstra( locationsList )
-		String [] minRoute = Dijkstra(locationsList);
+		// List<String> minRoute = new List(numLocations);
+		// minRoute = Dijkstra(locationsList);
 
 		//return String[] route representing the route the user should take, ex
 			//Grand Canyon AZ, Bemidji MN, New York NY, etc
-  		return minRoute;
+  		// return minRoute;
+  		// return locationsList;
 	}
 
-	public String[] attractionsLocations( String[] attractions, String[] locationsList ){
+	public List<String> attractionsLocations( List<String> attractions, List<String> locationsList ){
   		String csvSplit = ",";
 		String line = "";
 		BufferedReader attractionsFile = null;
@@ -39,9 +41,9 @@ public class leastDistanceRoute{
 				String[] attraction = line.split(csvSplit);
 				//attraction[0] = Event or place of interest, attraction[1] = location
 				//Loop through String[] attractions and finds the location of that attraction by reading attractions.csv and add them to locationsList
-				for (int i=0; i<attractions.length; i++){
-					if(attractions[i].equals(attraction[0])){
-						locationsList[i+1]=attraction[1];}
+				for (int i=0; i<attractions.getSize(); i++){
+					if(attractions.get(i).equals(attraction[0])){
+						locationsList.add((i+1), attraction[1]);}
 				}
 			}
 		} catch(FileNotFoundException e){
@@ -94,8 +96,9 @@ public class leastDistanceRoute{
 		return distanceMiles;
 	}
 
-	public String[] Dijkstra(String[] locationsList){
-	  	String [] routeDijkstra = new String[locationsList.length];
+	public List<String> Dijkstra(List<String> locationsList){
+		List<String>  routeDijkstra = new List(locationsList.getSize());
+	  	
 	  	//Vertex = locationsList
 	  	
 	  	//Known = start with all as false
@@ -105,7 +108,7 @@ public class leastDistanceRoute{
 		//Known=true and Cost=0
 		
 		//Cost = start with all cost=-1, then use calcDistance function to get costs: float cost = calcDistance(location1, location2)
-		int cost = calcDistance(locationsList[0], locationsList[1]);
+		int cost = calcDistance(locationsList.get(0), locationsList.get(1));
 		//Implement Dijkstra’s Algorithm until all Known are true and go backwards with Path and Vertex to create route and set to List<String> routeDijkstra
 		
 
@@ -130,20 +133,23 @@ public class leastDistanceRoute{
 	int numAttractions = Integer.valueOf(input.nextLine());
   	// Lists<leastDistanceRoute> temp = new ArrayList<>();
   	// ArrayList<String> attractions = new ArrayList<String>();
-  	List<String> attractions = minRoute.List();
-
-  	String [] attractions = new String [numAttractions];
+  	List<String> attractions = new List();
 
   	for (int i=0; i<numAttractions; i++){
   		System.out.println("Attraction "+(i+1)+" :");
   		String temp = input.nextLine();
-  		attractions[i] = temp;
+  		attractions.add(temp);
   	}
-  	//Create String[] bestRoute and store return value from route(starting_city, ending_city, attraction) function
-  	String [] bestRoute = minRoute.route(starting_city, ending_city, attractions);
-	//Print String[] bestRoute
-  	for (int i = 0; i<bestRoute.length; i++){
-			System.out.println(bestRoute[i]);
+
+  	// for (int i=0; i<numAttractions; i++){
+  	// 	System.out.println(attractions.get(i));
+  	// }
+  	//Create String bestRoute and store return value from route(starting_city, ending_city, attraction) function
+  	List <String> bestRoute = new List();
+  	bestRoute = minRoute.route(starting_city, ending_city, attractions);
+	//Print String bestRoute
+  	for (int i = 0; i<bestRoute.getSize(); i++){
+			System.out.println(bestRoute.get(i));
 	}
   }
 
