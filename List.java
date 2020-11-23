da88480038dce0666c5d0f5750bfc66c1004bab4
srtabarestarquinio <49605681+srtabarestarquinio@users.noreply.github.com>
2020-11-23 15:27:56 -0600

    Update List.java

--- List.java
+++ List.java
@@ -1,14 +1,6 @@
-// public interface List<E>{
-// 	E get(int position);
-// 	boolean add(E value);
-// 	E remove(int position);
-// 	int getSize();
-// }
+//Rose Tabares - CS245 - Assignment02 - Best Road Trip Route - List.java class
+
 public class List<E>{
-	// E get(int position);
-	// boolean add(E value);
-	// E remove(int position);
-	// int getSize();
 
 	E[] arr;
 	int size;
@@ -43,7 +35,6 @@ public class List<E>{
 			grow_list();
 		}
 		arr[size++]=value;
-		// return true;
 	}
 	public void add(int position, E value){
 		for(int i = size; i>position; i--){
@@ -63,115 +54,4 @@ public class List<E>{
 		size--;
 		return value;
 	}
-
 }
-// class LinkedList<E> implements List<E>{
-// 	Node<E> head;
-// 	int size;
-// 	private static class Node<E>{
-// 		E data;
-// 		Node<E> next;
-
-// 		public Node(E value){
-// 			data=value;
-// 			next=null;
-// 		}
-// 	}
-	
-// 	public LinkedList(){
-// 		head=null;
-// 		size=0;
-// 	}
-// 	public int getSize(){
-// 		return size;
-// 	}
-// 	//Get : This takes in an integer position which then returns the node at that position in the list. If the position is invalid, return null
-// 	public E get(int position){
-// 		//error check
-// 		if(position<0||position>=size){
-// 			System.out.println("Invalid index");
-// 			return null;
-// 		}
-
-// 		Node<E> curr = head;
-// 		for (int i = 0; i<position; i++){
-// 			curr = curr.next;
-// 		}
-// 		return curr.data;
-// 	}
-// 	public boolean add(E item){
-// 		if(head == null){
-// 			head = new Node<E>(item);
-// 			++size;
-// 			return true;
-// 		}
-// 		else{
-// 			Node<E> prev = head;
-// 			while(prev.next!= null){
-// 				prev=prev.next;
-// 			}
-// 			Node<E> node = new Node<E>(item);
-// 			prev.next= node;
-// 			++size;
-// 			return true;
-// 		}
-// 	}
-// 	public void add(E item, int position){
-// 		if(position == 0){
-// 			Node<E> node = new Node<E>(item);
-// 			node.next = head;
-// 			head = node;
-// 			++size;
-// 		}
-// 		else{
-// 			Node<E> prev = head;
-// 			for(int i=0; i<position-1; i++){
-// 				prev = prev.next;
-// 			}
-// 			Node<E> node = new Node<E>(item);
-// 			node.next = prev.next;
-// 			prev.next = node;
-// 			++size;
-// 		}
-// 	}
-// 	//Remove: The function takes in an integer position , which it then both removes and returns the ListNode at that position
-// 	public E remove(int position){
-// 		//Error check
-// 		if(position<0 || position>=size){
-// 			System.out.println("Invalid index");
-// 			return null;
-// 		}
-// 		if (position == 0){
-// 			Node<E> node = head;
-// 			head = head.next;
-// 			--size;
-// 			return node.data;
-// 		}
-// 		else{
-// 			Node<E> prev = head;
-// 			for (int i=0; i<position-1; i++){
-// 				prev = prev.next;
-// 			}
-// 			Node<E> node = prev.next;
-// 			prev.next = node.next;
-// 			--size;
-// 			return node.data;
-// 		}
-// 	}
-
-// 	//Reverse: The function reverses the whole linked list from head to tail. It should take in a parameter ListNode head. And return back the new head once the function is finished.
-// 	public Node<E> reverse(Node<E> head){
-// 		Node<E> prev=null;
-// 		Node<E> curr= head;
-// 		Node<E> next= null;
-// 		while(curr!=null){
-// 			next= curr.next;
-// 			curr.next=prev;
-// 			prev=curr;
-// 			curr=next;
-// 		}
-// 		head=prev;
-// 		return head;
-// 	}
-
-// }
