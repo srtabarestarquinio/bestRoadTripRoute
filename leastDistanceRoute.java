372b0ae7279d232aa610c380414110815cecb643
srtabarestarquinio <49605681+srtabarestarquinio@users.noreply.github.com>
2020-11-22 22:21:39 -0600

    Update leastDistanceRoute.java

--- leastDistanceRoute.java
+++ leastDistanceRoute.java
@@ -1,69 +1,118 @@
 //Rose Tabares - CS245 - Assignment02 - Best Road Trip Route
 import java.util.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.*;
+
+
 public class leastDistanceRoute{
-	//constructor:
-	public leastDistanceRoute(){
-
-	}
-	private Lists<leastDistanceRoute> list;
-	public leastDistanceRoute(Lists<leastDistanceRoute> newList){
-		list = newList;
-	}
   
-	public List<String> route(String starting_city, String ending_city, List<String> attractions){
- 	//Creates empty list of locations of size of List<String> attractions + 2 (for starting and ending city) called List<String> locationsList
-	
+	public String [] route(String starting_city, String ending_city, String [] attractions){
+ 		//Creates empty list of locations of size of List<String> attractions + 2 (for starting and ending city) called List<String> locationsList
+		int numLocations = attractions.length + 2;
+		String [] locationsList = new String [numLocations];
+		//Add starting_city in position 0 and ending_city in position -1 of locationsList attractionsLocations( attractions, locationsList )
+		locationsList[0] = starting_city;
+		locationsList[numLocations-1] = ending_city;
+		//add locations of attractions to locationList with function attractionslocations()
+		locationsList = attractionsLocations(attractions, locationsList);
+		
+		//Use Dijkstra’s algorithm to calculate shortest distances and add them to List<String> route = Dijkstra( locationsList )
+		String [] minRoute = Dijkstra(locationsList);
 
-	//Add starting_city in position 0 and ending_city in position -1 of locationsList attractionsLocations( attractions, locationsList )
-	
+		//return String[] route representing the route the user should take, ex
+			//Grand Canyon AZ, Bemidji MN, New York NY, etc
+  		return minRoute;
+	}
+
+	public String[] attractionsLocations( String[] attractions, String[] locationsList ){
+  		String csvSplit = ",";
+		String line = "";
+		BufferedReader attractionsFile = null;
+
+  		try{
+			attractionsFile = new BufferedReader(new FileReader("attractions.csv"));
+			while ((line = attractionsFile.readLine()) != null){
+				String[] attraction = line.split(csvSplit);
+				//attraction[0] = Event or place of interest, attraction[1] = location
+				//Loop through String[] attractions and finds the location of that attraction by reading attractions.csv and add them to locationsList
+				for (int i=0; i<attractions.length; i++){
+					if(attractions[i].equals(attraction[0])){
+						locationsList[i+1]=attraction[1];}
+				}
+			}
+		} catch(FileNotFoundException e){
+			System.out.println("File not found");
+		} catch(IOException e){
+			System.out.println("IOException");
+		} finally {
+			if (attractionsFile != null){
+				try{
+					attractionsFile.close();
+				} catch(IOException e){
+					System.out.println("IOException");
+				}
+			}
+		}
+		return locationsList;
+	}
+
+	public int calcDistance(String location1, String location2){
+	  	int distanceMiles = 0;
+	  	//use roads.csv to calculate distances between locations
+		String csvSplit = ",";
+		String line = "";
+		BufferedReader roadsFile = null;
+
+  		try{
+			roadsFile = new BufferedReader(new FileReader("roadss.csv"));
+			while ((line = roadsFile.readLine()) != null){
+				String[] road = line.split(csvSplit);
+				//road[0] = locations1, road[1] = locations2, road[2] = distance in miles
+				// Loop over roads.csv until find row that contains both locations and save the 3rd column to get the miles in between them
+				if((location1.equals(road[0]) && location2.equals(road[1])) || (location2.equals(road[0]) && location1.equals(road[1]))){
+					distanceMiles = Integer.valueOf(road[2]);
+				}
+			}
+		} catch(FileNotFoundException e){
+			System.out.println("File not found");
+		} catch(IOException e){
+			System.out.println("IOException");
+		} finally {
+			if (roadsFile != null){
+				try{
+					roadsFile.close();
+				} catch(IOException e){
+					System.out.println("IOException");
+				}
+			}
+		}
+		// return number of miles as distance
+		return distanceMiles;
+	}
+
+	public String[] Dijkstra(String[] locationsList){
+	  	String [] routeDijkstra = new String[locationsList.length];
+	  	//Vertex = locationsList
+	  	
+	  	//Known = start with all as false
+	 	
+	 	//Path = start with all as -1, then leave starting_city or locationsList[0] as -1 path because it will be starting point and all others will go from there, and set this with
+		
+		//Known=true and Cost=0
+		
+		//Cost = start with all cost=-1, then use calcDistance function to get costs: float cost = calcDistance(location1, location2)
+		int cost = calcDistance(locationsList[0], locationsList[1]);
+		//Implement Dijkstra’s Algorithm until all Known are true and go backwards with Path and Vertex to create route and set to List<String> routeDijkstra
+		
 
 
-	//Use Dijkstra’s algorithm to calculate shortest distances and add them to List<String> route = Dijkstra( locationsList )
-	
 
-
-	//return List<String> route representing the route the user should take, ex
-		//Grand Canyon AZ, Bemidji MN, New York NY, etc
-  
-  }
-
-  public void attractionsLocations( List<String> attractions, List<String> locationsList ){
-  	//Loops through List<String> attractions and finds the location of that attraction by reading attractions.csv and add them to locationsList
-  	for (int i=0; i<attractions.getSize(); i++){
-
-  	}
-  }
-
-  public float calcDistance(String location1, String location2){
-  	//use roads.csv to calculate distances between locations
-	
-
-	// Loop over roads.csv until find row that contains both locations and save the 3rd column to get the miles in between them
-	
-
-	// return number of miles as distance
-
-  }
-
-  public List<String> Dijkstra( List<String> locationsList ){
-  	//Vertex = locationsList
-  	
-  	//Known = start with all as false
- 	
- 	//Path = start with all as -1, then leave starting_city or locationsList[0] as -1 path because it will be starting point and all others will go from there, and set this with
-	
-	//Known=true and Cost=0
-	
-	//Cost = start with all cost=-1, then use calcDistance function to get costs: float cost = calcDistance(location1, location2)
-	
-	//Implement Dijkstra’s Algorithm until all Known are true and go backwards with Path and Vertex to create route and set to List<String> routeDijkstra
-	
-
-
-
-	//return routeDijkstra
-  }
+		return routeDijkstra;
+	}
 
   public static void main(String[] args){
  	//Take in starting, ending city and attraction
@@ -77,17 +126,25 @@ public class leastDistanceRoute{
   	String starting_city = input.nextLine();
   	System.out.println("Ending city:");
   	String ending_city = input.nextLine();
-  	
-  	Lists<leastDistanceRoute> temp = new ArrayList<String>();
+	System.out.println("Number of attractions: ");
+	int numAttractions = Integer.valueOf(input.nextLine());
+  	// Lists<leastDistanceRoute> temp = new ArrayList<>();
+  	// ArrayList<String> attractions = new ArrayList<String>();
+  	List<String> attractions = minRoute.List();
 
-	//Store attractions.csv and roads.csv in file variables
+  	String [] attractions = new String [numAttractions];
 
-
-	//Create List<String> route and store return value from route(starting_city, ending_city, attraction) function
-
-
-	//Print List<String> route
-  
+  	for (int i=0; i<numAttractions; i++){
+  		System.out.println("Attraction "+(i+1)+" :");
+  		String temp = input.nextLine();
+  		attractions[i] = temp;
+  	}
+  	//Create String[] bestRoute and store return value from route(starting_city, ending_city, attraction) function
+  	String [] bestRoute = minRoute.route(starting_city, ending_city, attractions);
+	//Print String[] bestRoute
+  	for (int i = 0; i<bestRoute.length; i++){
+			System.out.println(bestRoute[i]);
+	}
   }
 
 }
