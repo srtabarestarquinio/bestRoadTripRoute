dd848a7fcb3b8d29efe5c39166a4c4c0549eaa7d
srtabarestarquinio <49605681+srtabarestarquinio@users.noreply.github.com>
2020-11-22 19:13:26 -0600

    Create leastDistanceRoute.java

---
+++ leastDistanceRoute.java
@@ -0,0 +1,16 @@
+//Rose Tabares - CS245 - Assignment02 - Best Road Trip Route
+import java.util.*;
+
+public class leastDistanceRoute{
+
+  public List<String> route(String starting_city, String ending_city, List<String> attractions){
+  
+  
+  }
+
+  public static void main(String[] args){
+  
+  
+  }
+
+}
